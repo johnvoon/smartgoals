038a60856e8379c92194bb6e80128963710349b0
johnvoon <johnksvoon@gmail.com>
2017-08-11 09:13:12 +1000

    Adjusted formatting

--- notification_service.rb
+++ notification_service.rb
@@ -1,22 +1,24 @@
-class NotificationService
+module SmartGoals
+  class NotificationService
     
     attr_accessor :schedule
 
     def initialize
-        @scheduler = Rufus::Scheduler.new
+      @scheduler = Rufus::Scheduler.new
     end
 
     def message
-        
-        # Will run a notification every 3 seconds just as an example
-        @scheduler.every '3s' do #.at '2017/08/10 18:50:00' do
-            system("echo 'Hello Smart Goals!' | terminal-notifier -sound default")
-            #@scheduler.shutdown
-        end
-        @scheduler.join
+      
+      # Will run a notification every 3 seconds just as an example
+      @scheduler.every '3s' do #.at '2017/08/10 18:50:00' do
+        system("echo 'Hello Smart Goals!' | terminal-notifier -sound default")
+        #@scheduler.shutdown
+      end
+      @scheduler.join
     end
 
     def shutdown
-        @scheduler.shutdown
+      @scheduler.shutdown
     end
+  end
 end
