a777454230a3909607f3e75e72364b112b9ef950
Glenn Dimaliwat <glenn@Gurenax.local>
2017-08-11 15:34:17 +1000

    Updating task.rb and notification_service.rb

--- notification_service.rb
+++ notification_service.rb
@@ -1,24 +1,129 @@
-module SmartGoals
-  class NotificationService
-    
-    attr_accessor :schedule
+class NotificationService
+  
+  attr_accessor :schedule
 
-    def initialize
-      @scheduler = Rufus::Scheduler.new
-    end
+  # How to describe it
+  def initialize
+    @scheduler = Rufus::Scheduler.new
+  end
 
-    def message
+  # What can it do
+  # create notification service
+  # message: String
+  # start_time: Time
+  # notification_time: Time
+  # stop_schedule_now: Boolean (e.g. task has been completed or for one-off tasks)
+  def create_notification_service(message, start_time, notification_time, frequency)
+
+    puts "Message should fire up at #{notification_time.to_s}"
       
-      # Will run a notification every 3 seconds just as an example
-      @scheduler.every '3s' do #.at '2017/08/10 18:50:00' do
-        system("echo 'Hello Smart Goals!' | terminal-notifier -sound default")
-        #@scheduler.shutdown
-      end
-      @scheduler.join
-    end
+    
+    if frequency == :once # If it only happens once
+      puts "Inside Once"
+      
+      @scheduler.at notification_time.to_s do #e.g. at '2017/08/10 18:50:00 +10:00' do
+      #@scheduler.every '3s', :first => :now do
 
-    def shutdown
-      @scheduler.shutdown
+        # Send notification message
+        self.send_notification(message)
+        # One off. Kill immediately
+        @scheduler.shutdown
+      end
+
+    else
+      puts "Inside Every"
+      # Get frequency String for rufus compatibility
+      frequency_string = get_frequency_string(start_time, frequency)
+
+      # Get first time when the notification fires off
+      first_time = get_first_time(start_time, frequency)
+
+      if frequency_string != 'invalid'
+        @scheduler.every frequency_string, :first_at => first_time.to_s do
+          # Send notification message
+          self.send_notification(message)
+
+          # If end time has been reached
+          if Time.now.getlocal.to_s == notification_time.to_s      
+            @scheduler.shutdown
+          end
+        end
+      end
+
     end
+    # Don't use @scheduler.join to keep thread in background
+    #@scheduler.join # Adds the scheduler to the scheduler thread
+    
+  end
+
+  # Get Frequency String
+  def get_frequency_string(start_time, frequency)
+    frequency_string = ''
+    case frequency
+      when :every_minute
+        frequency_string = '1m'
+      when :hourly
+        frequency_string = '1h'
+      when :daily
+        frequency_string = '1d'
+      when :weekly
+        frequency_string = '1w'
+      when :monthly
+        frequency_string = "#{get_days_to_next_month(start_time)}d"
+      when :annualy
+        frequency_string = "#{get_days_to_next_year(start_time)}d"
+      else
+        frequency_string = 'invalid'
+    end
+    frequency_string
+  end
+
+  # Get days to next month
+  def get_days_to_next_month(start_time)
+      start_datetime = start_time.to_datetime
+      end_datetime = start_datetime >> 1
+      days = (end_datetime - start_datetime).to_i
+  end
+
+  # Get days to next year
+  def get_days_to_next_year(start_time)
+      start_datetime = start_time.to_datetime
+      end_datetime = start_datetime >> 12
+      days = (end_datetime - start_datetime).to_i
+  end
+
+  # Get First At Time - Time when the Notification first fires off
+  def get_first_time(start_time, frequency)
+    case frequency
+      when :every_minute
+        (start_time.to_datetime + (1/1440.0)).to_time # 1 minute
+      when :hourly
+        (start_time.to_datetime + (1/24.0)).to_time # 1 hour
+      when :daily
+        (start_time.to_datetime + 1.0).to_time # 1 day
+      when :weekly
+        (start_time.to_datetime + 7.0).to_time # 1 week
+      when :monthly
+        (start_time.to_datetime + get_days_to_next_month(start_time).to_f).to_time # 1 month
+      when :annualy
+        (start_time.to_datetime + get_days_to_next_year(start_time).to_f).to_time # 1 year
+    end
+  end
+
+  # Send notification message
+  def send_notification(message)
+    if OS.mac?
+      system("echo '#{message}' | terminal-notifier -sound default")
+    elsif OS.linux?
+      system("notify-send '#{message}'")
+    elsif OS.windows?
+      # TODO
+      # If windows
+    end
+  end
+
+  # Cancel notification service
+  def cancel_notification_service
+    @scheduler.shutdown # Kills the notification service
   end
 end
--- task.rb
+++ task.rb
@@ -2,34 +2,74 @@
 class Task
   # What it has
   # description : String
-  # start_date : Time
-  # target_date : Time
+  # start_time : Time
+  # target_time : Time
   # mark_complete : Boolean
   # completion_date : Time
   # status : Symbol :todo :completed :failed
-  # frequency : Symbol :off :daily :weekly :monthly :annual
-  # reminder_date : Time
-  # reminder : String
+  # frequency : Symbol :once :daily :weekly :monthly :annualy
+  # notifications : Array of NotificationService
+
   attr_accessor :description
-  attr_accessor :start_date
-  attr_accessor :target_date
+  attr_accessor :start_time
+  attr_accessor :target_time
   attr_accessor :mark_complete
   attr_accessor :completion_date
   attr_accessor :status
   attr_accessor :frequency
-  attr_accessor :reminder_date
-  attr_accessor :reminder
+  attr_accessor :notifications
 
   # How to describe it
-  def initialize(task)
-      @description = task[:description]  
-      @status = :todo # Status is set to a "todo" upon creation
-      @frequency = task[:frequency] || :off
-      @start_date = Time.now.getlocal
+  def initialize
+      @status = :todo # Default status is set to a "todo" upon creation
+      @start_time = Time.now.getlocal
+      @frequency = frequency
+      @notifications = []
   end
 
   # What can it do
   # TODO
   # send_message - reminder, notification, encouragement
+  
+  # 1 hour before (end time minus 1 hour)
+  def create_reminder_notification
+    puts "Creating reminder notification"
+    @reminder = NotificationService.new
+    
+    #reminder_time = self.get_reminder_hour(@target_time, 1) # Target Time minus 1 hour
+    
+    #For demo purposes
+    reminder_time = self.get_reminder_minute(@target_time, 1) # Target Time minus 1 minute
+
+    message = "This is a reminder for your task: #{@description}"
+    @reminder.create_notification_service(message, @start_time, reminder_time, @frequency)
+    @notifications << @reminder
+  end
+  
+  # Get reminder minute before target time
+  def get_reminder_minute(time, before_minutes)
+    (time.to_datetime - (before_minutes/1440.0)).to_time
+  end
+
+  # Get reminder hour before target time
+  def get_reminder_hour(time, before_hours)
+    (time.to_datetime - (before_hours/24.0)).to_time
+  end
+
+  # Failed notification (end time met)
+  def create_failed_notification
+      puts "Creating failed notification"
+      @failed_message = NotificationService.new
+      message = "You failed to achieve your task: #{@description}"
+      @failed_message.create_notification_service(message, @start_time, @target_time, @frequency)
+      @notifications << @failed_message
+  end
+
+  # Cancel all notifications
+  def remove_notifications
+    @notifications.each do |notification|
+      notification.cancel_notification_service
+    end
+  end
 
 end
