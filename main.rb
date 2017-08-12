0ab26f01abd8d143ca75c38bd5c967ff691ab825
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-08-13 09:08:40 +1000

    Added comments to main.rb to add descriptions for every gem and every class. Make it more readable for the rest of the team

--- main.rb
+++ main.rb
@@ -1,18 +1,18 @@
-require 'date'
-require 'time'
-require 'rufus-scheduler'
-require 'highline'
-require 'tty-prompt'
-require 'terminal-table'
-require 'os'
-require 'concurrent'
-require 'mail'
+require 'date'            # Include Date object
+require 'time'            # Include Time object
+require 'rufus-scheduler' # Gem to create a thread scheduler
+require 'highline'        # Gem to create better command line interface
+require 'tty-prompt'      # Gem to create better console prompt component
+require 'terminal-table'  # Gem to create tabled UI for the console
+require 'os'              # Gem to detect the user's operating system
+require 'concurrent'      # Gem that provides thread safe concurrency
+require 'mail'            # Gem to create e-mail messages
 
-require_relative 'helpers'
-require_relative 'goalsetter'
-require_relative 'goal'
-require_relative 'task'
-require_relative 'notification_service'
+require_relative 'helpers'    # Class which provides our validation rules, calculation and convertion
+require_relative 'goalsetter' # Class which runs our app and drives the menu
+require_relative 'goal'       # Class which creates tasks
+require_relative 'task'       # Class which contains the details of the tasks and creates a notification service
+require_relative 'notification_service' # Class which creates the scheduled popup messages and email messages
 
 # People need a SMART Goal Setter because they have Goals.
 # In every Goal, there are Tasks that need to be achieved/completed.
@@ -23,8 +23,15 @@ require_relative 'notification_service'
 # It guides users by asking the right questions in a very SMART way
 
 module SmartGoals
+  # Create a constant PROMPT instance
   PROMPT = TTY::Prompt.new
+
+  # Cretea a constant CLI instance
   CLI = HighLine.new
+  
+  # Create the GOALSEETTER instance
   GOALSETTER = GoalSetter.new
+  
+  # Execute the program
   GOALSETTER.run_program
 end
